647138b9633a3da219672a64d510ad9db96e17d7
ernestwwchin <ernestwwchin@gmail.com>
2012-12-08 15:23:29 +0800

    initial import (restructure)

--- dms/docrepo/src/main/java/org/tmx/docspace/repository/DocUserRepository.java
+++ dms/docrepo/src/main/java/org/tmx/docspace/repository/DocUserRepository.java
@@ -1,9 +1,6 @@
 package org.tmx.docspace.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.tmx.docspace.domain.DocUser;
 
